140e72352a5c905e7ea919bd160cb9e9ca32d2cb
Pratik Shinde <145910708+Pratikshinde55@users.noreply.github.com>
2025-01-06 13:57:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,24 +17,13 @@ data "aws_ami" "ami-id-ec2" {
      }
 }
 
-
-#resource "aws_instance" "pratik-ec2-resource" {
- #   ami = data.aws_ami.ami-id-ec2.id
-  #  instance_type = var.InstanceType
-  #  key_name = "psTerraform-key"
-  #  vpc_security_group_ids = [aws_security_group.Pratik-SG-block.id]
-  #  tags = {
-  #     Name = var.InstanceName
-   # }
-  #}
-
 resource "aws_instance" "pratik-ec2-resource" {
     ami = data.aws_ami.ami-id-ec2.id
     instance_type = var.InstanceType
     key_name = "psTerraform-key"
     vpc_security_group_ids = [aws_security_group.Pratik-SG-block.id]  
-    subnet_id = element(aws_subnet.ps-subnet.*.id, 0)   
-    associate_public_ip_address = true                   # public IP is assigned
+    subnet_id = element(aws_subnet.ps-subnet.*.id, 0)                                      ## Subnet referance give with elemnt because there is two subnet i created for single VPC.
+    associate_public_ip_address = true                                                     ## public IP is assigned
     tags = {
        Name = var.InstanceName
     }
@@ -114,12 +103,13 @@ resource "aws_internet_gateway" "mygateway-block" {
    }
 }
 
+############ AWS_subnet custom create with two subnet range 
 
 resource "aws_subnet" "ps-subnet" {
-   count = length(var.SubnetRange)
+   count = length(var.SubnetRange)                                     # Here i define count as lenght
    vpc_id = aws_vpc.Pratik-vpc-block.id
-   cidr_block = element(var.SubnetRange , count.index)
-   availability_zone = element(var.zoneRange , count.index)
+   cidr_block = element(var.SubnetRange , count.index)                 ## This element go to 1st var.subnetRange varibles and then increase as count.index, It means two times because two subnetRange i put.
+   availability_zone = element(var.zoneRange , count.index)            ##element --> var.zone.range --> count.index , This done because i define "count".
    map_public_ip_on_launch = true
    tags = {
        Name = "Subnet-${count.index + 1}"
@@ -138,7 +128,8 @@ variable "zoneRange" {
     default = ["ap-south-1a", "ap-south-1b"]
 } 
 
-##
+########## create custom route table with own VPC #####################
+
 resource "aws_route_table" "ps-route-block" {
      vpc_id = aws_vpc.Pratik-vpc-block.id
      
@@ -151,18 +142,17 @@ resource "aws_route_table" "ps-route-block" {
      }
 }
 
-### Provides a resource to create an association between a route table and
-## a subnet or a route table and an internet gateway or virtual private gateway.
+##################  Provides a resource to create an association between a route table and  a subnet or a route table and an internet gateway or virtual private gateway. ###############
 
 
 resource "aws_route_table_association" "pratik-route-asso-block" {
-      count = length(var.SubnetRange)
+      count = length(var.SubnetRange)                                           ## Here i dine count as lenght dynamic
       subnet_id = element(aws_subnet.ps-subnet.*.id , count.index)
       route_table_id = aws_route_table.ps-route-block.id
 }
 
 
-################# Security group
+#################  AWS_Security_group assigned with own VPC #############################
 
 resource "aws_security_group" "Pratik-SG-block" {
    name = "terraform-allow-ssh-ps${var.SG_NAME}"
@@ -196,12 +186,12 @@ variable "SG-TCP-ALLOW" {
    type = list(number)
    default = [22, 80, 8080, 443]
 }
+
 variable "SG_NAME" {
     default = "Pratik-SG"
 }
 
-
-#### Map
+###################  Terraform Map type is use to store multiple values at single variable, In output we get all data in only one call ##################
 
 variable "myMap" {
    type = map
@@ -217,7 +207,7 @@ output "Retrieve-info" {
     value = var.myMap
 }
 
-############ locals 
+############ Terraform locals helps to use Terraform function, but can't insert values while calling from module block.##########################
 
 locals {
    service_Name = "Pratik_Terraform"
@@ -230,13 +220,12 @@ output "lOCAL-out" {
    value = local.CurrentTime 
 }
 
+######### This variable helps to give button during apply terraform file for custom output #################
 
-   
 variable "Put-true-to-run" {
     type = bool
 }
 
 output "OutPutASK" {
-    #description = "This is Custom message \n for terraform apply command"
     value = var.Put-true-to-run == true? "hey WlC from Pratik" : "Bye..! meet next type "
 }
